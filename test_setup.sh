a858f3515c5aa3dcc8535c16074c77966144d8c8
Tom Dörr <tom.doerr@tum.de>
2020-09-13 23:54:20 +0200

    Add docker command

--- test_setup.sh
+++ test_setup.sh
@@ -3,12 +3,18 @@
 TEST_DIR_NAME='test_dir'
 REPO_NAME='/TecoGAN'
 LOG_PREFIX='log_runGan_'
+docker_run_co
 
 rm_test_dir() {
     if [[ $(pwd) =~ $REPO_NAME'$' ]]
     then
         rm -rf $TEST_DIR_NAME
     fi
+}
+
+run_in_docker() {
+    command_to_execute="$1"
+    docker run --gpus all -it --mount src=$(pwd),target=/TecoGAN,type=bind -w /TecoGAN tecogan_image bash -c "$command_to_execute"
 }
 
 rm_test_dir
@@ -19,7 +25,7 @@ docker build docker --no-cache -t tecogan_image
 
 for i in {0..2}
 do
-    python3 runGan.py $i 2>&1 | tee "$LOG_PREFIX""$i"
+    run_in_docker "python3 runGan.py $i 2>&1 | tee ""$LOG_PREFIX""$i"
 done
 for i in {0..2}
 do
