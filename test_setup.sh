6ad6ae284a254bde0d2bbf6b4eff7a40ad8fe6fe
Tom Dörr <tom.doerr@tum.de>
2020-09-15 03:05:11 +0200

    Add test cases dataloader, 3 and 4

--- test_setup.sh
+++ test_setup.sh
@@ -3,7 +3,7 @@
 TEST_DIR_NAME='test_dir'
 REPO_NAME='/TecoGAN'
 LOG_PREFIX='log_runGan_'
-docker_run_co
+log_file_array=()
 
 rm_test_dir() {
     if [[ $(pwd) =~ $REPO_NAME'$' ]]
@@ -17,6 +17,24 @@ run_in_docker() {
     docker run --gpus all -it --mount src=$(pwd),target=/TecoGAN,type=bind -w /TecoGAN tecogan_image bash -c "$command_to_execute"
 }
 
+run_in_docker_and_log() {
+    python_script_to_execute=$1
+    log_postfix=$2
+    log_file_name="$LOG_PREFIX""$log_postfix"
+    log_file_array+=(log_file_name)
+    run_in_docker "python3 $python_script_to_execute 2>&1 | tee $log_file_name"
+}
+
+print_all_log_endings() {
+    for file in log_file_array
+    do
+        echo  "
+        ========================
+        output file: $file"
+        tail $file
+    done
+}
+
 rm_test_dir
 remote_url="$(git config --get remote.origin.url)"
 git clone $remote_url $TEST_DIR_NAME
@@ -25,14 +43,16 @@ docker build docker --no-cache -t tecogan_image
 
 for i in {0..2}
 do
-    run_in_docker "python3 runGan.py $i 2>&1 | tee ""$LOG_PREFIX""$i"
-done
-for i in {0..2}
-do
-    file="$LOG_PREFIX""$i"  
-    echo  "
-    ========================
-    output file: $file"
-    tail $file
+    run_in_docker_and_log "runGan.py $i" "$i"
 done
 
+print_all_log_endings
+
+run_in_docker_and_log "dataPrepare.py --start_id 2000 --duration 120 --REMOVE --disk_path TrainingDataPath" "dataPrepare"
+
+for i in {3..4}
+do
+    run_in_docker_and_log "runGan.py $i" "$i"
+done
+
+print_all_log_endings
